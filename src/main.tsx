c99662ca256001131a203e89a5c5be9597d1c9a3
BharathWaj-K-R <ravikap0063@gmail.com>
2026-08-24 11:38:55 +0530

    feat: expose portfolio stack details

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { TooltipProvider } from "@/components/ui/tooltip"
 import App from "./App"
+import { StackDetails } from "./components/StackDetails"
 import "./index.css"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <TooltipProvider delayDuration={120}>
       <App />
+      <StackDetails />
     </TooltipProvider>
   </StrictMode>,
 )
